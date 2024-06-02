773f2cb38abd78061d1f684d726a10da1ef4f52e
Narthana Epa <narthana.epa@gmail.com>
2024-06-02 17:49:16 +1000

    Skip generate tests

--- default.nix
+++ default.nix
@@ -37,7 +37,8 @@ rustPlatform.buildRustPackage {
   # is excluded by default from Nix.
   checkPhase = ''
     RUST_BACKTRACE=full cargo test --all-features -- \
-      --skip cli::plugins::ls::tests::test_plugin_list_urls
+      --skip cli::plugins::ls::tests::test_plugin_list_urls \
+      --skip cli::generate
   '';
 
   meta = with lib; {
